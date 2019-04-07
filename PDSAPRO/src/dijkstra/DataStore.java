30a9ee9baa8412ce359b8abbfc25ff2b5eea4f7c
Waruna-Kaushalya <waruna95610@gmail.com>
2019-04-07 10:33:41 +0530

    Commit

--- PDSAPRO/src/dijkstra/DataStore.java
+++ PDSAPRO/src/dijkstra/DataStore.java
@@ -12,18 +12,19 @@ package dijkstra;
 public class DataStore {
 
     public static Graph.Edge[] GRAPH = {
-        new Graph.Edge("a", "h", 8),
-        new Graph.Edge("b", "h", 11),
-        new Graph.Edge("b", "c", 8),
-        new Graph.Edge("c", "i", 2),
-        new Graph.Edge("c", "d", 7),
-        new Graph.Edge("c", "f", 4),
-        new Graph.Edge("d", "f", 14),
-        new Graph.Edge("e", "f", 10),
-        new Graph.Edge("f", "g", 2),
-        new Graph.Edge("g", "h", 1),
-        new Graph.Edge("i", "g", 6),
-        new Graph.Edge("h", "i", 8),};
+      new Graph.Edge("Badulla", "Dehiwala", 8),
+new Graph.Edge("Kandy", "Dehiwala", 11),
+new Graph.Edge("Kandy", "Galle", 8),
+new Graph.Edge("Galle", "Halawata", 2),
+new Graph.Edge("Galle", "d", 7),
+new Graph.Edge("Galle", "Kottawa", 4),
+new Graph.Edge("d", "Kottawa", 14),
+new Graph.Edge("Panadura", "Kottawa", 10),
+new Graph.Edge("Kottawa", "Borella", 2),
+new Graph.Edge("Borella", "Dehiwala", 1),
+new Graph.Edge("Halawata", "Borella", 6),
+new Graph.Edge("Dehiwala", "Halawata", 8),
+    };
 
 //public static void ccccc(){
 }
